a1877443656e869cffa59bb9f6fb8f4f8172baf3
chingizagha <cingiz167@gmail.com>
2022-08-24 16:17:31 +0400

    Onion Arc added

---
+++ ECommerceAPI/Core/ECommerceAPI.Application/Abstractions/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using ECommerceAPI.Domain.Entities;
+
+namespace ECommerceAPI.Application.Abstractions
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
