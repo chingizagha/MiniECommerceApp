ee22e7ca251a17dfbc97f282813730e526e84822
chingizagha <cingiz167@gmail.com>
2022-08-25 12:58:20 +0400

    Generic Repository and ConnectionStr added

---
+++ ECommerceAPI/Infrastructure/ECommerceAPI.Persistence/Repositories/ReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class
+    {
+        private readonly ECommerceAPIDbContext _context;
+        public ReadRepository(ECommerceAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()
+            => Table;
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression)
+            => Table.Where(expression);
+
+        public Task<T> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression)
+            => await Table.FirstOrDefaultAsync(expression);
+    }
+}
--- ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
-﻿using ECommerceAPI.Application.Abstractions;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceAPI.API.Controllers
@@ -8,19 +7,6 @@ namespace ECommerceAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductService _productService;
 
-        public ProductsController(IProductService productService)
-        {
-            _productService = productService;
-        }
-
-
-        [HttpGet]
-        public IActionResult GetProducts()
-        {
-            var products = _productService.GetProducts();
-            return Ok(products);
-        }
     }
 }
